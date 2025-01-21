9b118e5e815b04ab6f68cbbe02e8d4644b15f63c
Peter Björkholm <peter.bjorkholm@molk.com>
2025-01-21 10:54:52 +0100

    Removed the working code

--- main.c
+++ main.c
@@ -2,13 +2,8 @@
 #include "gen_num.h"
 
 int main(){
-    for (int i = 0; i < 101; i++){
-        if (guess(i) == 0){
-            printf("%d", i);
-        }
-    }
+   
+   // Your code goes here
     
-
-
     return 0;
 }
